5a9840095d990ac9b10e7a57158e4e76f8004e91
huifang <592546574@qq.com>
2018-12-21 20:16:12 +0800

    footer拆分完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-<div>app</div>
+  <div>
+    <router-view/>
+    <FooterGuide/>
+  </div>
 </template>
 
 <script>
-
+import FooterGuide from './components/FooterGuide/FooterGuide'
 export default {
-
+  components:{
+    FooterGuide
+  }
 }
 </script>
 
