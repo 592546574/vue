1303b03a273a538baf688a4fcd45580794ac59fd
huifang <592546574@qq.com>
2018-12-23 08:25:48 +0800

    星星完成

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import NavHeader from './components/NavHeader/NavHeader'
 import store from './store'
+import NavHeader from './components/NavHeader/NavHeader'
+import Star from './components/Star/Star'
 //注册全局组件
 Vue.component('NavHeader',NavHeader)
-
+Vue.component('Star',Star)
 new Vue({
   el: '#app',
   render: h => h(App),
   router,
-  store
+  store,
+  Star
 })
